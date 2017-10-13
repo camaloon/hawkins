fcd89505ece1a786ef219247b6666c9f1ccf9716
Fernando Stecconi <nandilugio@gmail.com>
2017-10-13 12:06:07 +0200

    Finished routine to clean builds
    No need for the fix routine anymore. Just keeping it there for future
    reference.

--- dev/clean_db.js
+++ dev/clean_db.js
@@ -3,7 +3,7 @@
 var admin = require("firebase-admin");
 
 // Create/download from Firebase Console > User and admin permissions > Service accounts
-var serviceAccount = require("./hawkins-camaloon-0548347e24ee.json");
+var serviceAccount = require("../hawkins-camaloon-0548347e24ee.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -14,26 +14,31 @@ var db = admin.database();
 
 //// Cleaning routine /////////////////////////////////////////////////////////
 //
-// The following routine has to be completed to not only delete builds but also
-// examples and logs!!
+// Remove builds (including its logs and examples) older than `days_to_keep`
+// days.
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-// var days_to_keep = 15;
-// var d = new Date();
-// var keep_builds_to = d.setDate(d.getDate() - days_to_keep);
-// 
-// var to_delete_ref = db.ref("builds").orderByChild("startedAt").endAt(keep_builds_to) //.once("value", function(s) { console.log(Object.keys(s.val()).length) })
-// 
-// console.log("Querying the DB...")
-// to_delete_ref.once("value", function(snapshot) {
-//   console.log("Deleting...")
-//   snapshot.forEach(function(child_snapshot) {
-//     // TODO: Also delete nodes in /examples and /logs
-//     child_snapshot.ref.remove()
-//     console.log(".") // TODO: show the dot after all resources are deleted
-//   });
-// }).then(function() { console.log("Done!") });
+var days_to_keep = 15;
+var d = new Date();
+var keep_builds_to = d.setDate(d.getDate() - days_to_keep);
+
+var to_delete_ref = db.ref("builds").orderByChild("startedAt").endAt(keep_builds_to) //.once("value", function(s) { console.log(Object.keys(s.val()).length) })
+
+console.log("Querying the DB...")
+to_delete_ref.once("value", function(snapshot) { // snapshot: Hash of buildHAsh: buildObject(startedAt, finishedAt, status, push, worker)
+  console.log("Deleting...")
+  Object.keys(snapshot.val()).forEach(function(buildHash) {
+    console.log("Deleting ref: " + buildHash);
+    Promise.all([
+      db.ref("logs/" + buildHash).remove(),
+      db.ref("examples/" + buildHash).remove(),
+      db.ref("builds/" + buildHash).remove(),
+    ])
+    .then(function(){ console.log("Deleted ref: " + buildHash)})
+    .catch(console.log.bind(this, "Errorll!!"));
+  });
+}).then(function() { console.log("Done!") });
 
 //// FIX: delete orphan examples and logs /////////////////////////////////////
 //
@@ -52,49 +57,49 @@ var db = admin.database();
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-var recursiveProcessLogBatch = function(batchSize, fromHash) {
-  processLogBatch(batchSize, fromHash)
-    .then(recursiveProcessLogBatch.bind(null, batchSize));
-};
-
-var processLogBatch = function(batchSize, fromHash) {
-  console.log("Querying " + batchSize + " logs from " + fromHash + "...");
-  return db.ref("logs").orderByKey().startAt(fromHash).limitToFirst(batchSize).once("value")
-    .then(processLogBatchSnapshot)
-    .catch(errorHandler);
-};
-
-var processLogBatchSnapshot = function(logsSnapshot) {
-  console.log("Iterating over the " + logsSnapshot.numChildren() + " logs received...");
-  var lastLogKey = null;
-  logsSnapshot.forEach(function(logSnapshot) {
-    lastLogKey = logSnapshot.key;
-    processLogSnapshot(logSnapshot);
-  });
-  return lastLogKey;
-};
-
-var processLogSnapshot = function(logSnapshot) {
-  console.log("Processing log " + logSnapshot.key + "...");
-  db.ref("builds/" + logSnapshot.key).once("value", function(correspondingBuildSnapshot) {
-    if (correspondingBuildSnapshot.exists()) {
-      console.log("- Leaving:      " + logSnapshot.key);
-    } else {
-      console.log("- Deleting: (!) " + logSnapshot.key);
-      deleteResourcesFor(logSnapshot.key);
-    }
-  }, errorHandler);
-};
-
-var deleteResourcesFor = function(buildHash) {
-  db.ref("logs/" + buildHash).remove().catch(errorHandler);
-  db.ref("examples/" + buildHash).remove().catch(errorHandler);
-};
-
-var errorHandler = console.log.bind(this, "Errorll!!");
-
-
-
-console.log("Go!");
-recursiveProcessLogBatch(100, "");
+// var recursiveProcessLogBatch = function(batchSize, fromHash) {
+//   processLogBatch(batchSize, fromHash)
+//     .then(recursiveProcessLogBatch.bind(null, batchSize));
+// };
+//
+// var processLogBatch = function(batchSize, fromHash) {
+//   console.log("Querying " + batchSize + " logs from " + fromHash + "...");
+//   return db.ref("logs").orderByKey().startAt(fromHash).limitToFirst(batchSize).once("value")
+//     .then(processLogBatchSnapshot)
+//     .catch(errorHandler);
+// };
+//
+// var processLogBatchSnapshot = function(logsSnapshot) {
+//   console.log("Iterating over the " + logsSnapshot.numChildren() + " logs received...");
+//   var lastLogKey = null;
+//   logsSnapshot.forEach(function(logSnapshot) {
+//     lastLogKey = logSnapshot.key;
+//     processLogSnapshot(logSnapshot);
+//   });
+//   return lastLogKey;
+// };
+//
+// var processLogSnapshot = function(logSnapshot) {
+//   console.log("Processing log " + logSnapshot.key + "...");
+//   db.ref("builds/" + logSnapshot.key).once("value", function(correspondingBuildSnapshot) {
+//     if (correspondingBuildSnapshot.exists()) {
+//       console.log("- Leaving:      " + logSnapshot.key);
+//     } else {
+//       console.log("- Deleting: (!) " + logSnapshot.key);
+//       deleteResourcesFor(logSnapshot.key);
+//     }
+//   }, errorHandler);
+// };
+//
+// var deleteResourcesFor = function(buildHash) {
+//   db.ref("logs/" + buildHash).remove().catch(errorHandler);
+//   db.ref("examples/" + buildHash).remove().catch(errorHandler);
+// };
+// 
+// var errorHandler = console.log.bind(this, "Errorll!!");
+// 
+//
+//
+// console.log("Go!");
+// recursiveProcessLogBatch(100, "");
 
